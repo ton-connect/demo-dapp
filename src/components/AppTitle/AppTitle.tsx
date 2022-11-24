60d3b26b55210b68e6befc97bdf3eb518a920677
siandrev <andreev.sergey.i@yandex.ru>
2022-11-24 17:26:52 +0400

    remote debugger added

--- src/components/AppTitle/AppTitle.tsx
+++ src/components/AppTitle/AppTitle.tsx
@@ -1,5 +1,6 @@
 import { CHAIN } from '@tonconnect/sdk';
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { Debugger } from 'src/components/AppTitle/Debugger/Debugger';
 import { useTonWallet } from 'src/hooks/useTonWallet';
 import './style.scss';
 
@@ -10,11 +11,24 @@ const chainNames = {
 
 export function AppTitle() {
 	const wallet = useTonWallet();
+	const debuggerRef = useRef<{ open: () => void }>();
+
+	const [clicks, setClicks] = useState(0);
+
+	useEffect(() => {
+		if (clicks >= 5) {
+			debuggerRef.current!.open();
+			setClicks(0);
+		}
+	}, [clicks]);
 
 	return (
-		<div className="dapp-title">
-			<span className="dapp-title__text">My Dapp</span>
-			{wallet && <span className="dapp-title__badge">{chainNames[wallet.account.chain]}</span>}
-		</div>
+		<>
+			<div className="dapp-title" onClick={() => setClicks((x) => x + 1)}>
+				<span className="dapp-title__text">My Dapp</span>
+				{wallet && <span className="dapp-title__badge">{chainNames[wallet.account.chain]}</span>}
+			</div>
+			<Debugger ref={debuggerRef} />
+		</>
 	);
 }
