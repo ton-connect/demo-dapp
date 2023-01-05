17c116522d807709e5520fee8fd26395b4e94232
siandrev <andreev.sergey.i@yandex.ru>
2023-01-05 18:05:30 +0400

    github pages localstorage separator added

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
 import App from './App';
 import './index.scss';
+import './patch-local-storage-for-github-pages';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
